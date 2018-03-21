979894ecc6a10b01f48ade3219788c1fd1b12d3e
ldgardner <ldgardner@wpi.edu>
2018-03-20 22:28:08 -0400

    some dv stuff

--- src/dv.cpp
+++ src/dv.cpp
@@ -8,6 +8,7 @@
 #include <dirent.h>
 #include <sys/types.h>
 #include <vector>
+#include <string>
 #include <algorithm>
 #include <errno.h>
 #include <unistd.h>
@@ -15,5 +16,46 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
+    string fileName;
+    DIR* dumpdir;
+    struct dirent* dumpdirentry;
+    struct dirent *d;
+    vector<struct dirent*> dumpdirentries;
+    string dumpsterPath;
+    string dumpsterEnvVar = "DUMPSTER";
 
+    dumpsterPath = getenv(dumpsterEnvVar.c_str());
+
+    if (dumpsterPath.empty()) {
+        printf("No match for '%s' in environment\n", dumpsterPath.c_str());
+        exit(-1);
+    } else {
+        //printf("TEST");
+    }
+
+    if (argc < 2) {
+        perror("No File Argument given. use \"dv -h\" for help.");
+        exit(-2);
+    } else {
+        fileName = argv[1];
+    }
+
+    printf("input: %s", argv[1]);
+
+    printf("\noutput: ");
+
+    dumpdir = opendir(dumpsterPath.c_str());
+    if (dumpdir == nullptr) {
+        perror("open");
+        exit(-6);
+    }
+
+    d = readdir(dumpdir);
+    while (d) {
+        printf("\n%s", d->d_name);
+        if(fileName.compare( d->d_name)==0){
+            printf("\n Found the file.");
+        }
+        d = readdir(dumpdir);
+    }
 }
