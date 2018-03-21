75a10ba1a5fbb723ca4e05c7e6d9b2acf705426b
Wyatt Henke <pcdriverpro@gmail.com>
2018-03-20 21:17:54 -0400

    removed unfound include, renamed vars to make more sense, exiting with negative errors, used rename instead of link and unlink

--- src/rm.cpp
+++ src/rm.cpp
@@ -8,9 +8,9 @@
 #include <dirent.h>
 #include <sys/types.h>
 #include <vector>
-#include <zconf.h>
 #include <algorithm>
 #include <errno.h>
+#include <unistd.h>
 
 /* (shown here for reference)
 struct  dirent {
@@ -24,9 +24,12 @@ struct  dirent {
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    DIR* dump;
-    struct dirent* entry;
-    vector<struct dirent*> entries;
+    DIR* curdir;
+    DIR* dumpdir;
+    struct dirent* currentdirentry;
+    vector<struct dirent*> currentdirentries;
+    struct dirent* dumpdirentry;
+    vector<struct dirent*> dumpdirentries;
     vector<string> filesToRemove;
     string dumpsterPath;
     string dumpsterEnvVar = "DUMPSTER";
@@ -36,60 +39,84 @@ int main(int argc, char* argv[]) {
     
     if (dumpsterPath.empty()) {
         printf("No match for '%s' in environment\n", dumpsterPath.c_str());
-        exit(1);
+        exit(-1);
     } else {
 //        cout << dumpsterPath << endl;
     }
     
-    if (argc < 2) {
+    if (argc < 1) {
         perror("no arguments");
-        exit(2);
+        exit(-2);
     }
     
-    dump = opendir(workDir.c_str());
-    if (dump == nullptr) {
+    curdir = opendir(workDir.c_str());
+    if (curdir == nullptr) {
         perror("open");
-        exit(1);
+        exit(-3);
     }
     
-    entry = readdir(dump);
-    while (entry) {
-        entries.push_back(entry);
-        entry = readdir(dump);
+    currentdirentry = readdir(curdir);
+    while (currentdirentry) {
+        currentdirentries.push_back(currentdirentry);
+        currentdirentry = readdir(curdir);
+    }
+    
+    dumpdir = opendir(dumpsterPath.c_str());
+    if (dumpdir == nullptr) {
+        perror("open");
+        exit(-6);
+    }
+    
+    dumpdirentry = readdir(dumpdir);
+    while (dumpdirentry) {
+        dumpdirentries.push_back(dumpdirentry);
+        dumpdirentry = readdir(dumpdir);
     }
     
     for (int i = 1; i < argc; ++i) {
         filesToRemove.emplace_back(argv[i]);
     }
     
-    entries.erase(remove_if(entries.begin(), entries.end(), [&] (struct dirent* e) {
+    currentdirentries.erase(remove_if(currentdirentries.begin(), currentdirentries.end(), [&] (struct dirent* e) {
         for (const auto &i : filesToRemove) {
             if (i == e->d_name) {
                 return false;
             }
         }
         return true;
-    }), entries.end());
+    }), currentdirentries.end());
     
+    if (filesToRemove.size() != currentdirentries.size()) {
+        cout << "not all files found in current directory" << endl;
+        exit(-7);
+    }
     
-    for (const auto &i : entries) {
-        cout << i->d_name << endl;
+    for (const auto &i : currentdirentries) {
+//        cout << i->d_name << endl;
         
         string oldfile = workDir + "/" + i->d_name;
         string newfile = dumpsterPath + "/" + i->d_name;
         
-        if (link(oldfile.c_str(), newfile.c_str()) != 0) {
-            cout << "link error" << endl;
+        if (rename(oldfile.c_str(), newfile.c_str()) != 0) {
+            cout << "rename error" << endl;
             cout << errno << endl;
+            exit(-4);
         }
+    
+//        if (link(oldfile.c_str(), newfile.c_str()) != 0) {
+//            cout << "link error" << endl;
+//            cout << errno << endl;
+//            exit(-4);
+//        }
         
-        if (unlink(oldfile.c_str()) != 0) {
-            cout << "unlink error" << endl;
-            cout << errno << endl;
-            cout << workDir.append("/").append(i->d_name) << endl;
-        }
+//        if (unlink(oldfile.c_str()) != 0) {
+//            cout << "unlink error" << endl;
+//            cout << errno << endl;
+//            cout << workDir.append("/").append(i->d_name) << endl;
+//            exit(-5);
+//        }
     }
     
-    closedir(dump);
+    closedir(curdir);
     return 0;
 }
