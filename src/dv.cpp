c580f79d511b2ef5279151996454d7656eba056d
ldgardner <ldgardner@wpi.edu>
2018-03-20 21:24:24 -0400

    adding other files and their make setups

---
+++ src/dv.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Luke Gardner on 3/20/18.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <cstdlib>
+#include <dirent.h>
+#include <sys/types.h>
+#include <vector>
+#include <algorithm>
+#include <errno.h>
+#include <unistd.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+}
