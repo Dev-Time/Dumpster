d1ee58d4739112538d4f8c8bb83932833d810b53
Wyatt Henke <pcdriverpro@gmail.com>
2018-03-20 22:41:59 -0400

    begining to look for directories

--- src/rm.cpp
+++ src/rm.cpp
@@ -12,6 +12,7 @@
 #include <errno.h>
 #include <unistd.h>
 #include <cstring>
+#include <libgen.h>
 
 /* (shown here for reference)
 struct  dirent {
@@ -24,11 +25,11 @@ struct  dirent {
 
 using namespace std;
 
-bool contains(struct dirent* tofind, vector<struct dirent*> vector) {
+bool contains(char* tofind, vector<struct dirent*> vector) {
     bool found = false;
     
     for (auto &i : vector) {
-        if (strcmp(i->d_name, tofind->d_name) == 0) {
+        if (strcmp(i->d_name, tofind) == 0) {
             found = true;
             return found;
         }
@@ -90,29 +91,46 @@ int main(int argc, char* argv[]) {
         filesToRemove.emplace_back(argv[i]);
     }
     
-    currentdirentries.erase(remove_if(currentdirentries.begin(), currentdirentries.end(), [&] (struct dirent* e) {
-        for (const auto &i : filesToRemove) {
-            if (i == e->d_name) {
-                return false;
-            }
-        }
-        return true;
-    }), currentdirentries.end());
+//    currentdirentries.erase(remove_if(currentdirentries.begin(), currentdirentries.end(), [&] (struct dirent* e) {
+//        for (const auto &i : filesToRemove) {
+//            if (i == e->d_name) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }), currentdirentries.end());
+//
+//    if (filesToRemove.size() != currentdirentries.size()) {
+//        cout << "not all files found in current directory" << endl;
+//        exit(-7);
+//    }
     
-    if (filesToRemove.size() != currentdirentries.size()) {
-        cout << "not all files found in current directory" << endl;
-        exit(-7);
-    }
-    
-    for (const auto &i : currentdirentries) {
+    for (const auto &i : filesToRemove) {
 //        cout << i->d_name << endl;
         
-        if (contains(i, dumpdirentries)) {
-            exit(200);
+        if (!dirname(const_cast<char *>(i.c_str()))) {
+            cout << "arg is a folder" << endl;
+        }
+    
+        string newfile = dumpsterPath + "/" + i;
+        
+        if (contains((char *) i.c_str(), dumpdirentries)) {
+            bool fixed = false;
+            for (int j = 1; j < 10; ++j) {
+                string possibleNewName = string(i) + string(".") + to_string(j);
+                if (!contains(const_cast<char *>(possibleNewName.c_str()), dumpdirentries)) {
+                    newfile += "." + to_string(j);
+                    fixed = true;
+                    break;
+                }
+            }
+            if (!fixed) {
+                cout << "too many versions of file in dumpster" << endl;
+                exit(-8);
+            }
         }
         
-        string oldfile = workDir + "/" + i->d_name;
-        string newfile = dumpsterPath + "/" + i->d_name;
+        string oldfile = workDir + "/" + i;
         
         if (rename(oldfile.c_str(), newfile.c_str()) != 0) {
             cout << "rename error" << endl;
