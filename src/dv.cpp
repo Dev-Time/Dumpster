a431e9bd1e75509a46cfa58701c2a2214a732ab0
ldgardner <ldgardner@wpi.edu>
2018-03-21 20:32:12 -0400

    dv working

--- src/dv.cpp
+++ src/dv.cpp
@@ -8,21 +8,38 @@
 #include <dirent.h>
 #include <sys/types.h>
 #include <vector>
-#include <string>
 #include <algorithm>
 #include <errno.h>
 #include <unistd.h>
+#include <cstring>
+#include <libgen.h>
 
 using namespace std;
 
+bool contains(char* tofind, vector<struct dirent*> vector) {
+    bool found = false;
+
+    for (auto &i : vector) {
+        if (strcmp(i->d_name, tofind) == 0) {
+            found = true;
+            return found;
+        }
+    }
+    return found;
+}
+
 int main(int argc, char* argv[]) {
     string fileName;
+    DIR* curdir;
     DIR* dumpdir;
+    struct dirent* currentdirentry;
+    vector<struct dirent*> currentdirentries;
     struct dirent* dumpdirentry;
-    struct dirent *d;
     vector<struct dirent*> dumpdirentries;
+    vector<string> filesToRestore;
     string dumpsterPath;
     string dumpsterEnvVar = "DUMPSTER";
+    string workDir = get_current_dir_name();
 
     dumpsterPath = getenv(dumpsterEnvVar.c_str());
 
@@ -40,9 +57,20 @@ int main(int argc, char* argv[]) {
         fileName = argv[1];
     }
 
+    // TEST
     printf("input: %s", argv[1]);
 
-    printf("\noutput: ");
+    curdir = opendir(workDir.c_str());
+    if (curdir == nullptr) {
+        perror("open");
+        exit(-3);
+    }
+
+    currentdirentry = readdir(curdir);
+    while (currentdirentry) {
+        currentdirentries.push_back(currentdirentry);
+        currentdirentry = readdir(curdir);
+    }
 
     dumpdir = opendir(dumpsterPath.c_str());
     if (dumpdir == nullptr) {
@@ -50,12 +78,50 @@ int main(int argc, char* argv[]) {
         exit(-6);
     }
 
-    d = readdir(dumpdir);
-    while (d) {
-        printf("\n%s", d->d_name);
-        if(fileName.compare( d->d_name)==0){
-            printf("\n Found the file.");
-        }
-        d = readdir(dumpdir);
+    dumpdirentry = readdir(dumpdir);
+    while (dumpdirentry) {
+        dumpdirentries.push_back(dumpdirentry);
+        dumpdirentry = readdir(dumpdir);
     }
+
+    for (int i = 1; i < argc; ++i) {
+        filesToRestore.emplace_back(argv[i]);
+    }
+
+    for (const auto &i : filesToRestore) {
+//        cout << i->d_name << endl;
+
+        if (!dirname(const_cast<char *>(i.c_str()))) {
+            cout << "arg is a folder" << endl;
+        }
+
+        string newfile = workDir + "/" + i;
+
+        if (contains((char *) i.c_str(), currentdirentries)) {
+            bool fixed = false;
+            for (int j = 1; j < 10; ++j) {
+                string possibleNewName = string(i) + string(".") + to_string(j);
+                if (!contains(const_cast<char *>(possibleNewName.c_str()), currentdirentries)) {
+                    newfile += "." + to_string(j);
+                    fixed = true;
+                    break;
+                }
+            }
+            if (!fixed) {
+                cout << "too many versions of file in dumpster" << endl;
+                exit(-8);
+            }
+        }
+
+        string oldfile = dumpsterPath + "/" + i;
+
+        if (rename(oldfile.c_str(), newfile.c_str()) != 0) {
+            cout << "rename error" << endl;
+            cout << errno << endl;
+            exit(-4);
+        }
+    }
+    closedir(curdir);
+    closedir(dumpdir);
+    return 0;
 }
